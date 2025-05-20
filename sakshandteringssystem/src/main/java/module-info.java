a9fe0534b27705c2fee7b4ccd890a26e33b82e36
JHTgh <jorgenthorjussen@live.com>
2025-05-20 13:14:50 +0200

    BrukerDAO og rolle-enum
    Lagt til metoder for å administrere brukere i BrukerDAO.
    
    Lagt til enumet Rolle

--- sakshandteringssystem/src/main/java/module-info.java
+++ sakshandteringssystem/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.eksamen2025 {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
+    requires java.sql;
 
     opens com.eksamen2025 to javafx.fxml;
     exports com.eksamen2025;
