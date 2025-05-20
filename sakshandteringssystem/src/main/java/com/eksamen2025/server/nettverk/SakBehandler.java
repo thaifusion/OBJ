eea9ef9e49b02c52df6ab42038d9522b2a1eee32
Vibeke Wolden <vibwol@live.no>
2025-05-20 21:31:08 +0200

    eee
    eee

--- sakshandteringssystem/src/main/java/com/eksamen2025/server/nettverk/SakBehandler.java
+++ sakshandteringssystem/src/main/java/com/eksamen2025/server/nettverk/SakBehandler.java
@@ -29,44 +29,6 @@ public class SakBehandler implements Runnable {
         this.socket = socket;
     }
 
-<<<<<<< HEAD
-    @Override
-    public void run() {
-        try (
-            ObjectOutputStream ut = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream inn = new ObjectInputStream(socket.getInputStream())
-        ) {
-            while (true) {
-                Object mottatt;
-
-                try {
-                    mottatt = inn.readObject();
-                } catch (EOFException eof) {
-                    System.out.println("Klient er koblet ifra!");
-                    break;
-                }
-
-                System.out.println("Mottatt objekt: " + mottatt);
-
-                if (mottatt instanceof SocketRequest) {
-                    SocketRequest req = (SocketRequest) mottatt;
-
-                    // Her kan du legge inn logikk basert på f.eks. req.getType()
-                    SocketResponse svar = behandleRequest(req);
-                    ut.writeObject(svar);
-                    ut.flush();
-
-                } else if (mottatt instanceof String && mottatt.equals("exit")) {
-                    System.out.println("Klienten koblet fra.");
-                    break;
-                } else {
-                    System.out.println("Ugyldig forespørsel mottatt.");
-                }
-            }
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Feil under behandling: " + e.getMessage());
-            e.printStackTrace();
-=======
 public void run() {
     try (
         ObjectOutputStream ut = new ObjectOutputStream(socket.getOutputStream());
@@ -103,35 +65,11 @@ public void run() {
             } else {
                 ut.writeObject(new SocketResponse(false, "Ugyldig forespørsel"));
             }
->>>>>>> main
         }
         
     } catch (Exception e) {
         e.printStackTrace();
     }
-<<<<<<< HEAD
-
-    private SocketResponse behandleRequest(SocketRequest req) {
-    String type = req.getType();
-    Object data = req.getData();
-    String brukernavn = req.getBrukernavn();
-
-    switch (type) {
-        case "hentBrukere":
-            // Her kan du hente brukere fra database hvis du har det
-            return new SocketResponse(true, "Brukerliste sendt");
-
-        case "lagreSak":
-            // Du kan bruke `data` til å få tak i selve saken
-            // f.eks.: Sak sak = (Sak) data;
-            return new SocketResponse(true, "Saken er lagret");
-
-        default:
-            return new SocketResponse(false, "Ukjent forespørsel: " + type);
-    }
-    }
-=======
 }
     
->>>>>>> main
 }
