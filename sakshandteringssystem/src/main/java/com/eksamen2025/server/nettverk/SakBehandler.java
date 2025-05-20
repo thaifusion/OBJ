b76f65f93fa22c375e7dbe9f3f158bba684ec1ed
Vibeke Wolden <vibwol@live.no>
2025-05-20 18:13:05 +0200

    Update SakBehandler.java
    Nå funker det med flere brukere på  appen/kjøre GUI

--- sakshandteringssystem/src/main/java/com/eksamen2025/server/nettverk/SakBehandler.java
+++ sakshandteringssystem/src/main/java/com/eksamen2025/server/nettverk/SakBehandler.java
@@ -1,10 +1,12 @@
 package com.eksamen2025.server.nettverk;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+import com.eksamen2025.SocketRequest;
 import com.eksamen2025.SocketResponse;
 
 /**
@@ -20,26 +22,62 @@ public class SakBehandler implements Runnable {
         this.socket = socket;
     }
 
+    @Override
     public void run() {
-        try {
-
+        try (
             ObjectOutputStream ut = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream inn = new ObjectInputStream(socket.getInputStream());
+            ObjectInputStream inn = new ObjectInputStream(socket.getInputStream())
+        ) {
+            while (true) {
+                Object mottatt;
 
+                try {
+                    mottatt = inn.readObject();
+                } catch (EOFException eof) {
+                    System.out.println("Klient er koblet ifra!");
+                    break;
+                }
 
-            Object mottatt = inn.readObject();
-            System.out.println("Motatt objektet: " + mottatt);
+                System.out.println("Mottatt objekt: " + mottatt);
 
-            // For å snede et svar tilbake til klienten
-            SocketResponse svar = new SocketResponse(true, "Server har mottatt objektet");
-            ut.writeObject(svar);
-            
+                if (mottatt instanceof SocketRequest) {
+                    SocketRequest req = (SocketRequest) mottatt;
 
-            socket.close();
+                    // Her kan du legge inn logikk basert på f.eks. req.getType()
+                    SocketResponse svar = behandleRequest(req);
+                    ut.writeObject(svar);
+                    ut.flush();
+
+                } else if (mottatt instanceof String && mottatt.equals("exit")) {
+                    System.out.println("Klienten koblet fra.");
+                    break;
+                } else {
+                    System.out.println("Ugyldig forespørsel mottatt.");
+                }
+            }
         } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Feil under behandling av melding: " + e.getMessage());
+            System.out.println("Feil under behandling: " + e.getMessage());
             e.printStackTrace();
         }
     }
-    
+
+    private SocketResponse behandleRequest(SocketRequest req) {
+    String type = req.getType();
+    Object data = req.getData();
+    String brukernavn = req.getBrukernavn();
+
+    switch (type) {
+        case "hentBrukere":
+            // Her kan du hente brukere fra database hvis du har det
+            return new SocketResponse(true, "Brukerliste sendt");
+
+        case "lagreSak":
+            // Du kan bruke `data` til å få tak i selve saken
+            // f.eks.: Sak sak = (Sak) data;
+            return new SocketResponse(true, "Saken er lagret");
+
+        default:
+            return new SocketResponse(false, "Ukjent forespørsel: " + type);
+    }
+    }
 }
